e0e6dc30d8b961999113831f4b761ba392bd2abc
bhansen0091 <bhansen0091@yahoo.com>
2021-03-10 18:51:44 -0600

    assignment state update

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 
-
-
 class PersonCard extends Component {
     constructor(props){
         super(props);
+        this.state = {
+            ageCount: this.props.age
+        }
     }
 
     render() {
@@ -12,12 +13,18 @@ class PersonCard extends Component {
             <div className="wrapper">
                 <div className="personCard">
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.ageCount}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ this.increment }>Birthday Button For {this.props.firstName}</button>
                 </div>
             </div>
         );
     }
+
+    increment = () => {
+        this.setState({ageCount: this.state.ageCount + 1});
+    }
+
 }
 
 export default PersonCard;
